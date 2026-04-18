b9ad0b9c6e532a4523853bdbcbf965b148188f9c
Prashant Aryan <prashantsuryavanahi51781@gmail.com>
2026-04-18 12:14:28 +0530

    fixed backend url

--- client/src/AdminUploadPage.js
+++ client/src/AdminUploadPage.js
@@ -34,7 +34,7 @@ function AdminUploadPage() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/certificates/upload",
+      "https://your-backend.onrender.com/api/certificates/upload",
         formData
       );
 
